7d8fafc36369c51a93464163f32ac8850db5154f
azaraterochester <azarate@rochester.edu.co>
2019-02-18 09:51:06 -0500

    S17_L196

---
+++ S17_L196/colorGame.js
@@ -0,0 +1,24 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+
+console.log(squares);
+
+for(var i = 0; i < squares.length; i++){
+    //Add color
+    squares[i].style.backgroundColor = colors[i];
+    //Add event listener
+    squares[i].addEventListener('click',function(){
+        alert('clicked one square');
+    });
+}
