d4c1ca1cdcc46a375e4baedbb9f19ba5234e5929
azarateo <azarateo@gmail.com>
2023-04-17 08:58:49 +0800

    adding home to index.js and the home view to the folder. Also adding path and the view path using join

--- S38_L403/index.js
+++ S38_L403/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const app = express()
 const port = 3000
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'views'))
+
+//Creating home endpoint
+app.get('/',(req, res) => {
+    res.render('home')
+})
+
+//Listening on port 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//Connecting to the MongoDB database
 mongoose.connect('mongodb://localhost:27017/productsDB', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message))
