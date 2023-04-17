a41b6af52fab5fa2812861ab9188c5a6f6155db1
azarateo <azarateo@gmail.com>
2023-04-17 12:58:50 +0800

    adding endpoint to find by id, and showing in product/show

--- S38_L406/index.js
+++ S38_L406/index.js
@@ -18,6 +18,18 @@ app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', {products})
 })
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        console.log(product)
+        res.render('products/show', {product})
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
 //Listening on port 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
