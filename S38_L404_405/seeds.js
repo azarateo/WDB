113cebd44eeb57b61f3da2069a8a090df347d230
azarateo <azarateo@gmail.com>
2023-04-17 10:12:15 +0800

    seeding multiple products to the database

--- S38_L404_405/seeds.js
+++ S38_L404_405/seeds.js
@@ -5,10 +5,21 @@ mongoose.connect('mongodb://localhost:27017/productsDB', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message))
 
-const product1 = new Product({
-    name: 'Grapefruit',
-    price: 1.99,
-    description: 'fruit'
-})
+// const product1 = new Product({
+//     name: 'Grapefruit',
+//     price: 1.99,
+//     description: 'fruit'
+// })
 
-product1.save().then(p => console.log(p)).catch(e => console.log(e))
+// // product1.save().then(p => console.log(p)).catch(e => console.log(e))
+
+const seedProducts = [
+    { name: 'Grapefruit', price: 1.99, description: 'fruit' },
+    { name: 'Fairy Egplant', price: 1.00, description: 'vegetable' },
+    { name: 'Organic Godess Melon', price: 4.99, description: 'fruit' },
+    { name: 'Organic Mini Seedless Water Melon', price: 3.99, description: 'fruit' },
+    { name: 'Organic Celery', price: 1.50, description: 'vegetable' },
+    { name: 'Chocolate Whole Milk', price: 2.69, description: 'dairy' },
+]
+
+Product.insertMany(seedProducts).then(p => console.log(p)).catch(e => console.log(e))
