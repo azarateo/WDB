05d22d344be57935c2c75d4a3da696a87f3a2277
azarateo <azarateo@gmail.com>
2023-04-17 09:03:10 +0800

    Correcting the folder name

--- S38_L403/seeds.js
+++ S38_L403/seeds.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Product = require('./models/product')
+
 mongoose.connect('mongodb://localhost:27017/productsDB', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message))
