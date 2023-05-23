c7f4a40faea0de8403ff5174b726cce2f3304157
azarateo <azarateo@gmail.com>
2023-05-23 09:02:07 +0800

    creating a camp through /makecampground

--- S39/index.js
+++ S39/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const app = express()
 const port = 3000
+const Campground = require('./models/campground')
 
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -14,4 +19,12 @@ db.once("open", () => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' })
+    await camp.save()
+    res.send(camp)
+}
